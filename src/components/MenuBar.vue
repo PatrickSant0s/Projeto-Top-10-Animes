d8c5590bdfdff7f9b0e5fee5c5cad86ef749a4bb
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2023-12-27 14:48:14 -0300

    learning class

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul>
-      <li v-for="item in menuBar" :key="item.menu">
-        <a :href="item.href"></a>{{ item.menu }}
+      <li v-for="item in menuBar" :key="item.menu" class="menu-principal">
+        <a :href="item.href" class="links"></a>{{ item.menu }}
       </li>
     </ul>
   </div>
