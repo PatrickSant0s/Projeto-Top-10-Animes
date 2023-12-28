9fab6a1c251164fa9babc73533f1aa9876959f0c
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2023-12-28 11:18:23 -0300

    add Login/Password

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,6 +1,36 @@
-<template>
-    <div>Logou</div>
+
+   <template>
+    <div class="email">
+  <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      hide-details="auto"
+      label="Email address"
+      placeholder="johndoe@gmail.com"
+      type="email"
+    ></v-text-field>
+  </v-responsive>
+  
+  <div class="senha">
+   
+  <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      label="Password"
+      type="input"
+      hint="Enter your password to access this website"
+    ></v-text-field>
+  </v-responsive>
+
+  </div>
+  
+  </div>
 </template>
+
 
 
 <script>
@@ -10,4 +40,10 @@ export default {
 </script>
 
 
-sty
+<style scoped>
+.senha, .email{
+    background: rgb(197, 201, 204);
+    width: 400px;
+}
+
+</style>
