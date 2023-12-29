ee165ea6f40fe892d94c5c2c2484ae9100fc9463
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2023-12-29 11:32:52 -0300

    add userlogin

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,49 +1,20 @@
-
-   <template>
-    <div class="email">
-  <v-responsive
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-text-field
-      hide-details="auto"
-      label="Email address"
-      placeholder="johndoe@gmail.com"
-      type="email"
-    ></v-text-field>
-  </v-responsive>
-  
-  <div class="senha">
-   
-  <v-responsive
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-text-field
-      label="Password"
-      type="input"
-      hint="Enter your password to access this website"
-    ></v-text-field>
-  </v-responsive>
-
-  </div>
-  
-  </div>
+<template>
+  <div><UserLogin/></div>
 </template>
 
 
 
 <script>
+
+import UserLogin from '@/components/UserLogin.vue';
+
+
+
 export default {
-    name:"LoginPage"
+  components: { UserLogin },
+    name:"LoginPage",
+   
 }
+
 </script>
 
-
-<style scoped>
-.senha, .email{
-    background: rgb(197, 201, 204);
-    width: 400px;
-}
-
-</style>
