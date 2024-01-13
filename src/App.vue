67e3771fd8fdaa61ce88da4f83ab1158b9fac668
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-01-13 12:28:46 -0300

    add NewUser

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,27 @@ import MenuBar from "@/components/MenuBar.vue";
 export default {
   name: "App",
   components: { MenuBar },
- 
+
 
   computed: {
     users() {
       return users;
     },
   },
+
+  created(){
+    this.alreadyExistsUser()
+  },
   mounted() {
     setUsers(this.users);
-    
+
   },
+  methods: {
+    alreadyExistsUser() {
+      const user = JSON.parse(window.localStorage.getItem("user"));
+      if (!user) this.$router.push("/login");
+      this.$router.push("/")
+    }
+  }
 };
 </script>
