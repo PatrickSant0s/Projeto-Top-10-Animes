1579aa5f51e37e685ea6df5a57bda5688709d1a0
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-01-15 16:52:56 -0300

    add carousel

--- src/components/HomeCarousel.vue
+++ src/components/HomeCarousel.vue
@@ -1,40 +1,50 @@
 <template>
-  <v-carousel :show-arrows="false">
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-      cover
-    ></v-carousel-item>
+  <v-carousel cycle height="500" hide-delimiter-background show-arrows="hover">
+    <v-carousel-item v-for="(image, index) in sortedImages" :key="index">
+      <v-sheet height="100%">      
+        <a :href="image.link" target="_blank" rel="noopener noreferrer" class="carousel-link">
+          <div class="d-flex fill-height justify-center align-center position-relative">
+            <img :src="image.src" :alt="image.alt" class="carousel-image" />
+          </div>
+        </a>
+      </v-sheet>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
-
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
-      }
-    },
-  }
+import { images } from '@/utils/GaleriaFotos';
+
+export default {
+  data() {
+    return {
+      sortedImages: images.sort((a, b) => a.src.localeCompare(b.src)),
+    };
+  },
+};
 </script>
 
-
-
 <style scoped>
+.carousel-image {
+  width: 300px;
+    height: 300px;
+    border-radius: 16px;
+}
 
+.carousel-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.top-rank-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.top-rank {
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+}
 </style>
