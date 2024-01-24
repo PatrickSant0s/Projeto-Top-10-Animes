3ed66fd7a67c0dbbfd1c7827f236785c8c4cc015
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-01-24 14:31:03 -0300

    logging in with the new user

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,94 +1,69 @@
 <template>
- <div class="Menu-Principal"> <v-parallax
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-  >
-    <div class="d-flex flex-column fill-height justify-center align-center text-white" >
-      <h1 class="text-h4 font-weight-thin mb-4">
-        Vuetify
-      </h1>
-      <v-sheet class="bg-deep-purple pa-12" rounded>
-    <v-card class="mx-auto px-6 py-8" max-width="344">
-      <v-form v-model="form" @submit.prevent="onSubmit">
-        <v-text-field id="email"
-          v-model="email"
-          :readonly="loading"
-          :rules="[required]"
-          class="mb-2"
-          clearable
-          label="Email"
-        > </v-text-field>
+  <div class="Menu-Principal"> <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+        <h1 class="text-h4 font-weight-thin mb-4">
+          Vuetify
+        </h1>
+        <v-sheet class="bg-deep-purple pa-12" rounded>
+          <v-card class="mx-auto px-6 py-8" max-width="344">
+            <v-form v-model="form" @submit.prevent="onSubmit">
+              <v-text-field id="email" v-model="email" :readonly="loading" :rules="[required]" class="mb-2" clearable
+                label="Email"> </v-text-field>
 
-        <v-text-field
-          v-model="password"
-          :readonly="loading"
-          :rules="[required]"
-          clearable
-          label="Password"
-          placeholder="Enter your password"
-        ></v-text-field>
+              <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Password"
+                placeholder="Enter your password"></v-text-field>
 
-        <br />
+              <br />
 
-        <v-btn
-        @click="onSubmit"
-         @submit="onSubmit"
-          :disabled="!form"
-          :loading="loading"
-          block
-          color="success"
-          size="large"
-          type="submit"
-          variant="elevated"
-        >
-          Sign In
-        </v-btn>
-      </v-form>
-    </v-card>
-  </v-sheet>
-     
-    </div>
-  </v-parallax>
+              <v-btn @click="onSubmit" @submit="onSubmit" :disabled="!form" :loading="loading" block color="success"
+                size="large" type="submit" variant="elevated">
+                Sign In
+              </v-btn>
+            </v-form>
+          </v-card>
+        </v-sheet>
 
- 
-</div>
+      </div>
+    </v-parallax>
+
+
+  </div>
 </template>
 
 <script>
 
-import { users } from "../utils/Users";
-
 export default {
-    name:"UserLogin",
-    
-    data: () => ({
+  name: "UserLogin",
+
+  data: () => ({
     email: "",
     password: "",
     loading: false,
     menuBar: [
-        { menu: "Register", actionItem:() => this.$router.push("/Register") },
-      ],
-    
-    
+      { menu: "Register", actionItem: () => this.$router.push("/Register") },
+    ],
+
+
   }),
 
-    computed: {
-    users() {
-      return users;
+  computed: {
+    user() {
+
+      return [JSON.parse(window.localStorage.getItem("user"))]
     },
   },
   mounted() {
-    
-    
+
+
   },
   methods: {
     onSubmit(event) {
       event.preventDefault();
-      
-      if (!this.email || !this.password) {
-        alert('Preencha ambos os campos de email e senha.');
-        return;
-      }
-      const isValidUser = this.users.find(user => user.email === this.email && user.password === this.password);
+      if(!this.isFieldsValidation(this.email, this.password)) alert('Preencher ambos corretamente')
+
+        console.log(this.user[0])
+        const isValidUser = this.user.find(user => user.Email === this.email && user.Password === this.password);
+        
 
       if (isValidUser) {
         this.loading = true;
@@ -99,6 +74,17 @@ export default {
         this.$router.push('/Register').catch()
       }
     },
+
+    isFieldsValidation(email, password) {
+    console.log(email, password)
+     
+      if( !email || !password ) return false
+      else return true
+       
+
+
+
+    }
   },
 };
 </script>
