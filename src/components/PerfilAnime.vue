6654ff967d2dd3aad742843f229c3e19d1a505cf
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-02-23 11:01:51 -0300

    CREATING ANIME PROFILE

--- src/components/PerfilAnime.vue
+++ src/components/PerfilAnime.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="personagem" :style="{ backgroundImage: `url(${sortedImages[activeIndex].src})` }">
-    <div class="conteudo">
-      <i class="logo"></i>
-      <h2 class="nome-personagem">{{ sortedImages[activeIndex].alt }}</h2>
-      <p class="descricao">{{ sortedImages[activeIndex].descricao }}</p>
+  <main class="personagens">
+    <div class="personagem" v-for="(image, index) in sortedImages" :key="index">
+      <img class="imagem" src="" alt="" />
+      <div class="conteudo">
+        <i class="logo"></i>
+        <h2 class="nome-personagem">{{ image.alt }}</h2>
+        <p class="descricao">{{ image.descricao }}</p>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -14,49 +17,69 @@ export default {
   data() {
     return {
       sortedImages: images.sort((a, b) => a.src.localeCompare(b.src)),
-      activeIndex: 0,
     };
   },
 };
 </script>
 
 <style scoped>
+body {
+  background-color: #000000;
+  overflow: hidden;
+}
+
+main::after {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-height: 100vh;
+  width: 80vw;
+  background: linear-gradient(-233deg, #000 40%, rgba(0, 0, 0, 0) 65%) no-repeat;
+}
+
 .personagem {
-  position: relative; 
-  width: 100%; 
-  height: 100vh; 
-  background-size: cover; 
-  background-position: center; 
+  position: relative; /* Alterado para position: relative; */
+  height: 100vh;
+}
+
+.personagem .imagem {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 .conteudo {
-  position: absolute; /* Definindo a posição como absoluta */
-  top: 50%; /* Alinha o conteúdo no meio verticalmente */
-  left: 50%; /* Alinha o conteúdo no meio horizontalmente */
-  transform: translate(-50%, -50%); /* Move o conteúdo de volta para cima e para a esquerda em 50% da largura e altura do próprio conteúdo */
-  text-align: center; /* Centraliza o texto */
-  color: white; /* Cor do texto */
-  background-color: rgba(0, 0, 0, 0.5); /* Fundo semitransparente */
-  padding: 20px; /* Espaçamento interno */
-}
-.conteudo .nome-personagem {
-    color: #ffffff;
-    font-size: 48px;
-    font-family: "Secular One";
-    font-weight: 400;
-    margin-bottom: 20px;
-}
-.conteudo .descricao {
-    color: #ffffff;
-    font-family: "Rubik";
-    line-height: 24px;
-}
-.conteudo .logo {
-    background-image: url("https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/e6b2cd29a5ff62f4591d3b299007e24e.jpe");
-    background-size: cover;
-    height: 100px;
-    width: 230px;
-    margin-bottom: 35px;
+  position: absolute;
+  top: 0;
+  left: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  max-width: 380px;
+  z-index: 1;
 }
 
+.conteudo .nome-personagem {
+  color: #ffffff;
+  font-size: 48px;
+  font-family: "Secular One";
+  font-weight: 400;
+  margin-bottom: 20px;
+}
+
+.conteudo .descricao {
+  color: #ffffff;
+  font-family: "Rubik";
+  line-height: 24px;
+}
+
+.conteudo .logo {
+  background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Naruto_logo.svg/1200px-Naruto_logo.svg.png);
+  background-size: cover;
+  height: 100px;
+  width: 230px;
+  margin-bottom: 35px;
+}
 </style>
