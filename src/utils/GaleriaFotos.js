36539ee78e0f2e27fe3e7ef8a0ecdb4bfc067746
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-02-29 10:34:49 -0300

    addjusting card

--- src/utils/GaleriaFotos.js
+++ src/utils/GaleriaFotos.js
@@ -5,7 +5,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=Qno808u9NEY",
     descricao:
       "A trama segue Luffy, um jovem com o corpo elástico que sonha em criar sua própria tripulação e partir em uma aventura para se tornar o próximo Rei dos Piratas. Ao todo, a produção tem oito episódios de cerca de uma hora de duração cada, já disponíveis no catálogo da Netflix.",
-    temporada: "20 Temporadas",
     gif: "https://i.pinimg.com/originals/71/36/26/7136261341c8133bd2b68b188055bf06.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/7.png",
     id: 8,
@@ -17,7 +16,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=AhG2dKqunLQ&t=12s",
     descricao:
       "Naruto Uzumaki é um ninja hiperativo da Vila Oculta da Folha e membro da equipe 7, filho do Quarto Hokage e Kushina Uzumaki. Logo após seu nascimento, a Raposa de Nove Caudas foi selada dentro dele, mas isso foi mantido em segredo por anos.",
-    temporada: "9 Temporadas",
     gif: "https://i.pinimg.com/originals/61/b5/c3/61b5c31b0ed70fc8aacd9eaa511c2949.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/1.png",
     id: 1,
@@ -29,7 +27,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=jQT2s9kaLBw",
     descricao:
       "A trama de Yu Yu Hakusho gira em torno de Yusuke Urameshi, um jovem delinquente sem rumo na vida, acostumado a causar confusão na sua escola e vizinhança. Um dia, ele vê um garotinho prestes a ser atropelado e, agindo por instinto, se joga na frente do carro.",
-    temporada: "1 Temporadas",
     gif: "https://i.gifer.com/8Kpp.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/5.png",
     id: 6,
@@ -41,7 +38,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=EzN5SW2QOWY&list=PL-QeKcXki44guOnuBGWoZEMRA8BNtxVRC",
     descricao:
       "Criada por Akira Toryiama, a franquia conta a história de Son Goku, guerreiro que descobre ser parte de um legado de poderosos conquistadores alienígenas - e passa a defender seu planeta adotivo, a Terra, de outros seres igualmente superfortes e capazes de feitos descomunais.",
-    temporada: "9 Temporadas",
     gif: "https://media1.tenor.com/m/Z3DYCffBSt4AAAAC/super-saiyan3-goku.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/2.png",
     id: 2,
@@ -53,7 +49,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=SSViT336Vq4&list=PL5Yff77BRE_rJQkYlaIOYVOgJv1jRr9d2",
     descricao:
       "Ao chegar atrasado ao Laboratório do Professor Samuel Carvalho, o pesquisador local de Pokémon, Ash descobre que os três últimos Pokémon iniciais de Kanto (Bulbasaur, Charmander e Squirtle) já foram doados a outros treinadores. O único Pokémon disponível é um Pikachu, que o Professor Carvalho gentilmente oferece a Ash. A partir desse momento, Ash e Pikachu iniciam sua jornada juntos, enfrentando desafios, fazendo novos amigos e perseguindo seu sonho de se tornar um Mestre Pokémon.",
-    temporada: "8 Temporadas",
     gif: "https://i.pinimg.com/originals/8a/81/ec/8a81ecd8fdd266b3221da325875c0ea8.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/6.png",
     id: 7,
@@ -65,7 +60,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=4gV24yiLiwQ",
     descricao:
       "A história acompanha um grupo de crianças que são levadas ao Digimundo, um mundo digital habitado por criaturas conhecidas como Digimon. Lá, elas devem juntar-se às criaturas e lutar contra um mal iminente que ameaça o mundo paralelo",
-    temporada: "4 Temporadas",
     gif: "https://i.pinimg.com/originals/cd/0e/c8/cd0ec868095e60f8b688163619d29654.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/8.png",
     id: 5,
@@ -77,7 +71,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=IX1aSD_pZh0",
     descricao:
       "Em Kimetsu no Yaiba, Tanjiro, um bondoso jovem que ganha a vida vendendo carvão descobre que sua família foi massacrada por um demônio. E, para piorar, Nezuko, sua irmã mais nova e única sobrevivente, também acabou transformada em um demônio.",
-    temporada: "4 Temporadas",
     gif: "https://i.makeagif.com/media/6-02-2023/zHdFsZ.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/3.png",
     id: 3,
@@ -89,7 +82,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=yI6v5-61VRU",
     descricao:
       "Na história, o Death Note é um caderno, normalmente de capa preta, e que tem seu nome escrito na parte frontal dele. Esse caderno tem a capacidade de matar qualquer pessoa apenas se os nomes destas pessoas forem escritos nele, enquanto o portador visualizar mentalmente o rosto de quem quer assassinar.",
-    temporada: "2 Temporadas",
     gif: "https://i.makeagif.com/media/9-09-2021/Yo9pTl.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/10.png",
     id: 10,
@@ -101,7 +93,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=Nds9oPVLATc",
     descricao:
       "Os Cavaleiros do Zodíaco são guerreiros que juram proteger a deusa Atena das forças do mal - para isso, canalizam em golpes os poderes do cosmo. Ao todo são 88 cavaleiros, representantes de cada uma das 88 constelações oficiais.",
-    temporada: "3 Temporadas",
     gif: "https://i.pinimg.com/originals/f6/1c/c3/f61cc3ff83a4ef755b6d53a782460189.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/9.png",
     id: 9,
@@ -113,7 +104,6 @@ export const images = [
     link: "https://www.youtube.com/watch?v=nm5dTf_8oio",
     descricao:
       "InuYasha (犬夜叉), também conhecido como Inuyasha: Um Conto de Fadas Feudal (戦国御伽草子 犬夜叉 Sengoku Otogizōshi InuYasha), é uma série de mangá shōnen escrita e ilustrada por Rumiko Takahashi. Foi publicada na revista Weekly Shōnen Sunday entre 13 de novembro de 1996 e 18 de junho de 2008, totalizando 56 volumes tankōbon.",
-    temporada: "7 Temporadas",
     gif: "https://www.icegif.com/wp-content/uploads/2022/04/icegif-172.gif",
     rank: "https://www.netflix.com/tudum/top10/images/big_numbers/4.png",
     id: 4,
--- src/utils/GaleriaPerfil.js
+++ src/utils/GaleriaPerfil.js
@@ -4,10 +4,13 @@ export const animes = [
     imagem:
       "https://i.pinimg.com/originals/83/a9/f6/83a9f6bbc742a977b87c4fe14fa1fef2.jpg",
     nome: "Naruto Uzumaki",
+    
     descricao:
       "Naruto Uzumaki é um ninja hiperativo da Vila Oculta da Folha e membro da equipe 7, filho do Quarto Hokage e Kushina Uzumaki. Logo após seu nascimento, a Raposa de Nove Caudas foi selada dentro dele, mas isso foi mantido em segredo por anos.",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Naruto_logo.svg/1200px-Naruto_logo.svg.png",
     link: "https://www.youtube.com/watch?v=AhG2dKqunLQ&t=12s",
+    temporada: "9 Temporadas",
+
   },
 
   {
@@ -19,6 +22,8 @@ export const animes = [
       "Criada por Akira Toryiama, a franquia conta a história de Son Goku, guerreiro que descobre ser parte de um legado de poderosos conquistadores alienígenas - e passa a defender seu planeta adotivo, a Terra, de outros seres igualmente superfortes e capazes de feitos descomunais.,",
     logo: "https://img.asmedia.epimg.net/resizer/P1CLFc7X1QLdkD1BhWpp9kwT3xo=/1472x828/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/XV7QUOH5WFC53I4HMXUXKEKHNA.jpg",
     link: "https://www.youtube.com/watch?v=EzN5SW2QOWY&list=PL-QeKcXki44guOnuBGWoZEMRA8BNtxVRC",
+    temporada: "9 Temporadas",
+
   },
   {
     id: 3,
@@ -29,6 +34,8 @@ export const animes = [
       "Em Kimetsu no Yaiba, Tanjiro, um bondoso jovem que ganha a vida vendendo carvão descobre que sua família foi massacrada por um demônio. E, para piorar, Nezuko, sua irmã mais nova e única sobrevivente, também acabou transformada em um demônio.",
     logo: "https://fontswan.com/wp-content/uploads/Demon-Slayer-Font.webp",
     link: "https://www.youtube.com/watch?v=IX1aSD_pZh0",
+    temporada: "4 Temporadas",
+
   },
   {
     id: 4,
@@ -39,6 +46,7 @@ export const animes = [
       "InuYasha (犬夜叉), também conhecido como Inuyasha: Um Conto de Fadas Feudal (戦国御伽草子 犬夜叉 Sengoku Otogizōshi InuYasha), é uma série de mangá shōnen escrita e ilustrada por Rumiko Takahashi. Foi publicada na revista Weekly Shōnen Sunday entre 13 de novembro de 1996 e 18 de junho de 2008, totalizando 56 volumes tankōbon.",
     logo: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABdsrzPrNTNDCRY2AyvvUGLszD3THGX1xKLvXWW9fCCIq1rEbVTmEBh3G-ZGgaZlPFtfqqDmxADp4vWOVc0QYK4febTGX2skPYYUD4KPJiLnB.png?r=a54",
     link: "https://www.youtube.com/watch?v=nm5dTf_8oio",
+    temporada: "7 Temporadas",
   },
   {
     id: 5,
@@ -48,6 +56,8 @@ export const animes = [
       "A história acompanha um grupo de crianças que são levadas ao Digimundo, um mundo digital habitado por criaturas conhecidas como Digimon. Lá, elas devem juntar-se às criaturas e lutar contra um mal iminente que ameaça o mundo paralelo",
     logo: "https://i.ytimg.com/vi/Dd-rd3y6KUg/hqdefault.jpg",
     link: "https://www.youtube.com/watch?v=4gV24yiLiwQ",
+    temporada: "4 Temporadas",
+
   },
   {
     id: 6,
@@ -57,6 +67,8 @@ export const animes = [
       "A trama de Yu Yu Hakusho gira em torno de Yusuke Urameshi, um jovem delinquente sem rumo na vida, acostumado a causar confusão na sua escola e vizinhança. Um dia, ele vê um garotinho prestes a ser atropelado e, agindo por instinto, se joga na frente do carro.",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Y%C5%AB_Y%C5%AB_Hakusho_logo.svg/800px-Y%C5%AB_Y%C5%AB_Hakusho_logo.svg.png",
     link: "https://www.youtube.com/watch?v=jQT2s9kaLBw",
+    temporada: "1 Temporadas",
+
   },
   {
     id: 7,
@@ -66,6 +78,8 @@ export const animes = [
       "Pokémon é um grande anime, com centenas de episódios, além de alguns filmes e especiais, e continua tendo sucesso no Japão e no mundo. O protagonista do desenho, Ash, viaja ao redor do mundo Pokémon para ganhar insígnias ao derrotar os líderes de ginásio de várias cidades, na intenção de participar das Ligas Pokémon.",
     logo: "https://logosmarcas.net/wp-content/uploads/2020/05/Pokemon-Logo.png",
     link: "https://www.youtube.com/watch?v=SSViT336Vq4&list=PL5Yff77BRE_rJQkYlaIOYVOgJv1jRr9d2",
+    temporada: "8 Temporadas",
+
   },
   {
     id: 8,
@@ -76,6 +90,8 @@ export const animes = [
       "A trama segue Luffy, um jovem com o corpo elástico que sonha em criar sua própria tripulação e partir em uma aventura para se tornar o próximo Rei dos Piratas. Ao todo, a produção tem oito episódios de cerca de uma hora de duração cada, já disponíveis no catálogo da Netflix.",
     logo: "https://logosmarcas.net/wp-content/uploads/2021/10/One-Piece-Logo.png",
     link: "https://www.youtube.com/watch?v=Qno808u9NEY",
+    temporada: "20 Temporadas",
+
   },
   {
     id: 9,
@@ -86,6 +102,8 @@ export const animes = [
       "Os Cavaleiros do Zodíaco são guerreiros que juram proteger a deusa Atena das forças do mal - para isso, canalizam em golpes os poderes do cosmo. Ao todo são 88 cavaleiros, representantes de cada uma das 88 constelações oficiais.",
     logo: "https://seeklogo.com/images/C/cavaleiros-do-zodiaco-logo-7BEBD54081-seeklogo.com.png",
     link: "https://www.youtube.com/watch?v=Nds9oPVLATc",
+    temporada: "3 Temporadas",
+
   },
   {
     id: 10,
@@ -96,5 +114,7 @@ export const animes = [
       "Na história, o Death Note é um caderno, normalmente de capa preta, e que tem seu nome escrito na parte frontal dele. Esse caderno tem a capacidade de matar qualquer pessoa apenas se os nomes destas pessoas forem escritos nele, enquanto o portador visualizar mentalmente o rosto de quem quer assassinar.",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Death_Note_logo_%28black_background%29.png/1200px-Death_Note_logo_%28black_background%29.png",
     link: "https://www.youtube.com/watch?v=yI6v5-61VRU",
+    temporada: "2 Temporadas",
+
   },
 ];
